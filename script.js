28888d368a1d2d4002b1682b5934b6e262400e87
Griffins Ngeno <griffins.ngeno@student.moringaschool.com>
2025-01-28 12:33:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const bars = document.getElementById('bars');
     const menu = document.getElementById('menu');
 
-    // Toggle the mobile menu when the hamburger icon is clicked
     bars.addEventListener('click', function () {
         menu.classList.toggle('active');
     });
 
-    // Close the mobile menu when a link is clicked
+   
     const menuLinks = document.querySelectorAll('.menu li a');
     menuLinks.forEach(link => {
         link.addEventListener('click', function () {
-            menu.classList.remove('active'); // Hide the menu after clicking a link
+            menu.classList.remove('active'); 
         });
     });
+});
+
+let swiper = new Swiper('.mySwiper', {
+    loop: true,  
+    navigation: {
+        nextEl: '.next',  
+        prevEl: '.prev',  
+    },
+    pagination: {
+        el: '.swiper-pagination',  
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,  
+        disableOnInteraction: false,  
+    }
 });
