588e6ef78c49e7e811e8665f15eabeab071b5ffc
Griffins Ngeno <griffins.ngeno@student.moringaschool.com>
2025-01-29 13:51:25 +0300

    update

--- script.js
+++ script.js
@@ -16,18 +16,24 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-let swiper = new Swiper('.mySwiper', {
-    loop: true,  
-    navigation: {
-        nextEl: '.next',  
-        prevEl: '.prev',  
-    },
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
     pagination: {
-        el: '.swiper-pagination',  
-        clickable: true,
+      el: '.swiper-pagination',
     },
-    autoplay: {
-        delay: 3000,  
-        disableOnInteraction: false,  
-    }
-});
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
